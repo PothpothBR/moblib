e9c3ccb9e7b0c1018cd16902bf7f9551e95249f5
PothpothBR <dalagnolbruno88@gmail.com>
2022-03-09 16:50:58 -0300

    Mover os backup de forma a ficar mais organizado.

--- moblib/backup[0-0-0](old python)/cardgame.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-pygame.init()
-
-screen = pygame.display.setmode(1080,720)
-
-pygame
--- moblib/backup[2020-4-12]/moblib/mapping.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAPPING_H
-#define MAPPING_H
-
-#include "debug_log.h"
-typedef unsigned short int usint;
-typedef  short int sint;
-
-#endif
